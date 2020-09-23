9efd59ffc6fbfc680d5163111370eec15bafe1a9
Aleš Roubíček <ales@roubicek.name>
2020-09-23 06:51:05 +0200

    Cleanup, logging, browser action

--- extension/background.js
+++ extension/background.js
@@ -1,9 +1,24 @@
-chrome.pageAction.onClicked.addListener(function (tab) {
-  chrome.runtime.sendMessage({ action: "Download" }, () =>
-    fetch(new URL("nemocnice", "https://www.hlidacstatu.cz/api/v2/"), {
-      method: "POST",
-      body: JSON.stringify(message),
-      headers: new Headers([["Content-Type", "application/json"]]),
-    })
-  );
+const apiV2 = new URL("https://www.hlidacstatu.cz/api/v2/");
+
+chrome.browserAction.onClicked.addListener((tab) => {
+  chrome.tab.sendMessage(tab.id, { action: "Download" }, async (message) => {
+    console.group("Hlídač Státu");
+    console.log("Got response", message);
+    try {
+      const resp = await fetch(new URL("nemocnice", apiV2), {
+        method: "POST",
+        body: JSON.stringify(message),
+        headers: new Headers([["Content-Type", "application/json"]]),
+      });
+      if (!resp.ok) {
+        console.warn("Sending failed", resp.statusText);
+      } else {
+        console.log("All done", resp.statusText);
+      }
+    } catch (err) {
+      console.error(err);
+    } finally {
+      console.groupEnd();
+    }
+  });
 });
--- extension/content.js
+++ extension/content.js
@@ -1,18 +1,34 @@
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   if (message.action !== "Download") return;
 
-  let resp = await fetch("/prehled/api/regions", {
-    credentials: "include",
-    referrerPolicy: "origin",
-    headers: new Headers([["Accept", "application/json"]]),
-  });
+  try {
+    console.group("Hlídač Státu");
+    if (location.toString() === "https://dip.mzcr.cz/dip-prihlaseni") {
+      console.warn("Nejste přihlášeni");
+      return;
+    }
 
-  if (!resp.ok) return;
-  let data = await resp.json();
+    const resp = await fetch("/prehled/api/regions", {
+      mode: "same-origin",
+      credentials: "same-origin",
+      referrerPolicy: "same-origin",
+      headers: new Headers([["Accept", "application/json"]]),
+    });
 
-  console.group("Hlídač Státu");
-  console.log(data)
-  console.groupEnd();
+    if (!resp.ok) {
+      console.warn("Dotaz se nezdařil");
+      return;
+    }
+    const data = await resp.json();
 
-  return sendResponse(data);
+    console.log("Posíláte nám následující data:");
+    console.log(data);
+    console.log("Děkujeme");
+
+    return sendResponse(data);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    console.groupEnd();
+  }
 });
